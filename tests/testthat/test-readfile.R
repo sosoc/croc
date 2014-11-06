80fcf09e691fccab4c797257900853dc45100f28
Michael Sumner <mdsumner@gmail.com>
2014-11-05 22:09:47 -1000

    example files

--- tests/testthat/test-readfile.R
+++ tests/testthat/test-readfile.R
@@ -1,9 +1,10 @@
 context("Basic read")
 
-ftest1 <- system.file("extdata", "S2008001.L3b_DAY_CHL.main", package = "roc")
-ftest2 <- system.file("extdata", "S2010006.L3b_DAY_RRS.main", package = "roc")
+ftest1 <- system.file("extdata", "ocfiles", "S2008001.L3b_DAY_CHL.main", package = "roc")
+ftest2 <- system.file("extdata", "ocfiles", "S2010006.L3b_DAY_RRS.main", package = "roc")
 test_that("file read is successful", {
-   expect_that(readL3(ftest1),is_a("list"))
-   expect_that(names(readL3(ftest2))[21L], equals("Rrs_670_ssq"))
+  expect_that(file.exists(ftest1), is_true()) 
+  expect_that(readL3(ftest1),is_a("list"))
+  expect_that(names(readL3(ftest2))[21L], equals("Rrs_670_ssq"))
  })
  
