f616bb132531f9668b95ed41e40a6edb9c0a15d1
Michael Sumner <mdsumner@gmail.com>
2014-12-01 16:22:24 -1000

    check for aux files (needs work but most should be done by raadsync)

--- R/readL3.R
+++ R/readL3.R
@@ -8,6 +8,7 @@
 readL3 <- function(x, vname, bins = TRUE) {
   if (length(x) > 1L) warning("only first file considered")
   x <- iz2(x[1])
+  tst <- .checkAux(x)
   vdatalist <- vdatainfo(x)
   if (missing(vname)) {
     vdatalist <- vdatalist[!names(vdatalist) %in% c("SEAGrid", "BinList", "BinIndex")]
@@ -20,7 +21,28 @@ readL3 <- function(x, vname, bins = TRUE) {
   bl
 }
 
-
+.checkAux <- function(x) {
+  ## if this needs aux files, we better ensure they are present and decompressed
+  ## aux periods
+  ##c("MO", "R32", "YR", "SNWI")
+  ## What is MC, WC for SeaWiFS?  http://oceandata.sci.gsfc.nasa.gov/SeaWiFS/L3BIN/1998/001/
+  
+  ## season days 
+  ## SeaWiFS?  080 (SCSP) (seasonal composite?)
+  ## 080 (SNSP), 172 (SNSU), 264 (SNAU), 355 (SNWI)
+  ## MODIS
+  ## RRS:  x00:x11
+  ## NSST: x00
+  ## SST:  x00
+  ## SST4: x00
+  ## CU (only for SST4 in 20020012006181): x00 ?why
+  
+  ## this is very *&*^ complicated, just check if aux are present and fail if compressed
+  otherfiles <- unique(gsub(".bz2$", "", list.files(dirname(x), pattern = "\\.x0")))
+  if (!all(file.exists(file.path(dirname(x), otherfiles)))) stop("not all auxiliary files uncompressed: ", x)
+  NULL
+  
+}
 ## this really needs testing
 ## now that I've turned off file delete
 iz2 <- function(x) {
